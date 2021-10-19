25d437b46e877988080de9412bb7a30341f4e2ad
Lívia Nascimento <liviapnd@gmail.com>
2021-10-19 14:12:05 -0300

    primeiras funcionalidades

--- gallows.py
+++ gallows.py
@@ -5,21 +5,57 @@ def play():
     print("************************")
 
     word = "arroz"
+    guesses = []
+    letters = ['_', '_', '_', '_', '_']
+    user_word = "".join(letters)
 
     hanged = False
     hit = False
+    tries = len(letters) + 3
 
-    while(not hanged and not hit):      
+    print("Você tem {} tentativas".format(tries))
+    print("Boa sorte!\n")      
+    
+    while(not hanged and not hit):
+
+        print(user_word)
 
         guess = input("chute uma letra:".strip())
-        
-        index = 0
+        print("\n")
 
-        for letter in word:
-            if (letter.lower() == guess.lower()):
-                print(letter)
+        if (guess in guesses):
+            print("Você já chutou essa letra! Tente outra")
+            tries -= 1
+            print('Você tem {} tentativas\n'. format(tries))
+        elif (guess == ""):
+            print("Chute vazio :P escreva uma letra na próxima")
+            tries -= 1
+            print('Você tem {} tentativas\n'. format(tries))
+        else:
+            guesses.append(guess)
 
-        index += 1
-    
+            index = 0
+
+            for letter in word:
+                if (letter.lower() == guess.lower()):
+                    letters[index] = guess
+                    user_word = "".join(letters)    
+
+                index += 1
+            
+            tries -= 1
+            print('Você tem {} tentativas\n'. format(tries))
+
+        hanged = tries == 0
+        hit = word == user_word
+
+    if (hanged == True):
+        print("VOCÊ FOI ENFORCADO X_X")
+        print("MAIS SORTE NA PRÓXIMA VEZ")
+
+    if (hit == True):
+        print("CONSEGUIU SALVAR SEU PESCOÇO, MAS FOI PURA SORTE")
+        print("TE ESPERO DE NOVO")
+
 if(__name__ == "__main__"):
     play()
